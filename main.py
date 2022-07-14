206d184e384bd4674d8a37d53f5aba403508ecdc
trevorschul <105797203+trevorschul@users.noreply.github.com>
2022-07-14 13:03:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ for (index, row) in student_data_frame.iterrows():
 
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
-print(nato_dictionary)
+# print(nato_dictionary)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
